de676752819ceb8e5b0e9322863141ddb48ab169
Axel <axelmontes92@gmail.com>
2017-05-13 18:13:58 -0500

    avances

--- js/main.js
+++ js/main.js
@@ -13,17 +13,24 @@ $(window).on('load', function() {
     /*Tooltip*/
 		$('.tool').tooltip();
 
+
+
 		$('.nav a').click(function(){
 			$('.navbar-ex1-collapse').collapse('hide');
 		});
 
     /*Waypoint*/
+    $('#quienes-somos').css('opacity', 0);
 		$('.ser-general').css('opacity', 0);
 		$('.mmv').css('opacity', 0);
 
+		$('#quienes-somos').waypoint(function() {
+			$('#quienes-somos').addClass('fadeInUp');
+		}, { offset: '70%' });
+
 		$('.ser-general').waypoint(function() {
 			$('.ser-general').addClass('bounceIn');
-		}, { offset: '50%' });
+		}, { offset: '70%' });
 
 		$('.mmv').waypoint(function() {
 			$('.mmv').addClass('bounceIn');
